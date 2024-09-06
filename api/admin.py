ab6b4a815f2e607538db5c7ad351e8782d3700a0
jvictor-sec <negocios.jvictor@proton.me>
2024-09-06 10:06:45 -0300

    feat: add post and category models to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category
+
+@admin.register(Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'slug', 'author')
+    prepopulated_fields = { 'slug': ('title',), }
+
+admin.site.register(Category)
