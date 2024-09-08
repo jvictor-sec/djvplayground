7070696143477599ccbc53493b4cd7fac259990f
jvictor-sec <negocios.jvictor@proton.me>
2024-09-08 10:39:33 -0300

    feat: add permissions control

--- api/views.py
+++ api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import PostUserWritePermission
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     
