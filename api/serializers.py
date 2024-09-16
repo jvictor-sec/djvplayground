a7feedaef233bc19e70a8f6e5c45a2470df8e4c8
jvictor-sec <negocios.jvictor@proton.me>
2024-09-16 10:47:29 -0300

    feat: add jwt support

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,23 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import User, Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'user_name', 'first_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
+
+        if password is not None:
+            user.set_password(password)
+        
+        user.save()
+
+        return user
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
-    path('posts/<int:pk>/', PostDetail.as_view())
+    path('posts/<int:pk>/', PostDetail.as_view()),
+    path('get-token/', TokenObtainPairView.as_view()),
+    path('refresh-token/', TokenRefreshView.as_view())
 ]
