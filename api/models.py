8ffce1b6b6f0b694bc6b58713d783bba225fc902
jvictor-sec <negocios.jvictor@proton.me>
2024-09-16 10:45:41 -0300

    feat: add custom user model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,62 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.utils import timezone
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class CustomUserManager(UserManager):
+        def _create_user(self, email, user_name, first_name, password, **extra_fields):
+            if not email:
+                raise ValueError('You must provide an email address')
+
+            email = self.normalize_email(email)
+
+            user = self.model(email=email, user_name=user_name, first_name=first_name, **extra_fields)
+            user.set_password(password)
+            user.save()
+
+            return user
+
+        def create_user(self, email=None, user_name=None, first_name=None, password=None, **extra_fields):
+            extra_fields.setdefault('is_staff', False)
+            extra_fields.setdefault('is_superuser', False)
+            extra_fields.setdefault('is_active', True)
+
+            return self._create_user(email, user_name, first_name, password, **extra_fields)
+        
+        def create_superuser(self, email=None, user_name=None, first_name=None, password=None, **extra_fields):
+            extra_fields.setdefault('is_staff', True)
+            extra_fields.setdefault('is_superuser', True)
+            extra_fields.setdefault('is_active', True)
+
+            if extra_fields.get('is_staff') is not True:
+                raise ValueError('Superuser must be staff')
+            
+            if extra_fields.get('is_superuser') is not True:
+                raise ValueError('Superuser must be superuser')
+
+            return self._create_user(email, user_name, first_name, password, **extra_fields)
+
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=125, unique=True)
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+    about = models.TextField(max_length=500, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', 'first_name']
+
+    def __str__(self):
+        return self.user_name
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
